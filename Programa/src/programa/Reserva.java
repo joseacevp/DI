ca3fa4de0a936f8a3f5f0b38a527263ea2f4b0ae
Jose Ace <joseacevp@gmail.com>
2023-01-28 12:33:31 +0100

    Reinicio Tarea 1
    Inicio de implementación para comprobar si los datos introducidos en el formulario son validos.
    Ensayo para el examen de Febrero 2023
    Pide reconocer si los datos son EMAIL o numero.

---
+++ Programa/src/programa/Reserva.java
@@ -0,0 +1,119 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package programa;
+
+import java.util.Date;
+
+/**
+ *
+ * @author josea
+ */
+public class Reserva {
+
+    private String nombre;
+    private String telefono;
+    private Date fecha;
+    private String t_evento;
+    private String t_comida;
+    private boolean cheff;
+    private int asistentes;
+    private int jornadas;
+    private int habitaciones;
+
+    public Reserva() {
+    }
+
+    public Reserva(String nombre, String telefono, Date fecha, String t_evento, String t_comida, boolean cheff, int asistentes, int jornadas, int habitaciones) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.fecha = fecha;
+        this.t_evento = t_evento;
+        this.t_comida = t_comida;
+        this.cheff = cheff;
+        this.asistentes = asistentes;
+        this.jornadas = jornadas;
+        this.habitaciones = habitaciones;
+    }
+
+    @Override
+    public String toString() {
+        return "Reserva{" + "nombre=" + nombre + ", telefono=" + telefono + ", fecha=" + fecha + ", t_evento=" + t_evento + ", t_comida=" + t_comida + ", cheff=" + cheff + ", asistentes=" + asistentes + ", jornadas=" + jornadas + ", habitaciones=" + habitaciones + '}';
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getT_evento() {
+        return t_evento;
+    }
+
+    public void setT_evento(String t_evento) {
+        this.t_evento = t_evento;
+    }
+
+    public String getT_comida() {
+        return t_comida;
+    }
+
+    public void setT_comida(String t_comida) {
+        this.t_comida = t_comida;
+    }
+
+    public boolean isCheff() {
+        return cheff;
+    }
+
+    public void setCheff(boolean cheff) {
+        this.cheff = cheff;
+    }
+
+    public int getAsistentes() {
+        return asistentes;
+    }
+
+    public void setAsistentes(int asistentes) {
+        this.asistentes = asistentes;
+    }
+
+    public int getJornadas() {
+        return jornadas;
+    }
+
+    public void setJornadas(int jornadas) {
+        this.jornadas = jornadas;
+    }
+
+    public int getHabitaciones() {
+        return habitaciones;
+    }
+
+    public void setHabitaciones(int habitaciones) {
+        this.habitaciones = habitaciones;
+    }
+
+   
+    
+}
